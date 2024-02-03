f98e3131f806d9bf86e24d0a046b4a3d68995364
PavelLogeiko <114857717+PavelLogeiko@users.noreply.github.com>
2024-02-03 14:45:23 +0300

    Update UserActionAspect.java

--- src/main/java/com/example/Spring_HW08/aspect/UserActionAspect.java
+++ src/main/java/com/example/Spring_HW08/aspect/UserActionAspect.java
@@ -3,6 +3,7 @@ package com.example.Spring_HW08.aspect;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -20,6 +21,7 @@ Date currentDateAndTime = new Date();
 вывод на консоль логов действия пользователя
  */
     @After("@annotation(com.example.Spring_HW08.aspect.TrackUserAction)")
+    @Order(1)
     public void afterUserAction(JoinPoint joinPoint) {
         System.out.println("Действие пользователя: " + joinPoint.getSignature().getName() +
                 " | " + currentDateAndTime);
